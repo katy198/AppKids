e9a06e5780e6442e902f99c0e2dfa47a9e1ba529
katy198 <katy.pey@mail.ru>
2020-07-01 23:57:12 +0300

    Hello World + mysql configuration

---
+++ src/test/java/com/application/appkids/AppkidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.appkids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppkidsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
